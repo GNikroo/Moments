898cd77db37a7e27f84f297eaef7270b77cca788
GNikroo <gina.nikroo@gmail.com>
2022-12-13 09:36:13 +0000

    Add sign in form and functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults';
 import styles from "./App.module.css";
 import NavBar from './components/NavBar';
+import SignInForm from "./pages/auth/SignInForm";
 import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Container className={styles.Main}>
             <Switch>
                 <Route exact path='/' render={() => <h1>Home page</h1>} />
-                <Route exact path='/signin' render={() => <h1>Sign in </h1>} />
+                <Route exact path='/signin' render={() => <h1><SignInForm /></h1>} />
                 <Route exact path='/signup' render={() => <h1><SignUpForm /></h1>} />
                 <Route render={() => <p>Page not found!</p>} />
             </Switch>
