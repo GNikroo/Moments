f917c31d4322f32e91e50ca8054fc38e8d380968
GNikroo <gina.nikroo@gmail.com>
2023-01-04 11:52:34 +0000

    Set up post page to display posts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar';
 import SignInForm from "./pages/auth/SignInForm";
 import SignUpForm from "./pages/auth/SignUpForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                     <Route exact path='/signin' render={() => <h1><SignInForm /></h1>} />
                     <Route exact path='/signup' render={() => <h1><SignUpForm /></h1>} />
                     <Route exact path='/posts/create' render={() => <h1><PostCreateForm /></h1>} />
+                    <Route exact path='/posts/:id' render={() => <PostPage />} />
                     <Route render={() => <p>Page not found!</p>} />
                 </Switch>
             </Container>
